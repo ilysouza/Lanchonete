8814c13deed4a4ff104f85d6df5773d6f93c18a4
ilysouza <ily.souza@outlook.com>
2026-02-24 09:24:24 -0300

    Adicionando CRUD de cliente

---
+++ app/Livewire/ClienteCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ClienteCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.cliente-create');
+    }
+}
---
+++ app/Livewire/ClienteEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ClienteEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.cliente-edit');
+    }
+}
